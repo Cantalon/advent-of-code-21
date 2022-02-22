064004271ec139421e1a63703462d6f6fe57f799
Cantalon <99934780+Cantalon@users.noreply.github.com>
2022-02-22 09:20:39 -0800

    Create day2b.java

---
+++ day2b.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class day2b {
+
+    static final int inputLength = 1000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("input.txt"));
+        StringTokenizer st;
+        //BufferedReader is just a fast way to read input from a file
+
+        int x_pos = 0;
+        int y_pos = 0;
+        int aim = 0;
+
+        for (int i = 0; i < inputLength; i++) {
+            st = new StringTokenizer(r.readLine());
+
+            String command = st.nextToken();
+            int amount = Integer.parseInt(st.nextToken());
+
+            if (command.equals("forward")) {
+                x_pos += amount;
+                y_pos += aim * amount;
+            } else if (command.equals("down")) {
+                aim += amount;
+            } else if (command.equals("up")) {
+                aim -= amount;
+            }
+        }
+
+        System.out.println(x_pos * y_pos);
+        r.close();
+    }
+}
