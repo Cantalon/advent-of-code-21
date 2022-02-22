f6a09e444537dffa3a0db462d76b29b907994a03
Cantalon <99934780+Cantalon@users.noreply.github.com>
2022-02-21 17:26:15 -0800

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class day1 {
+
+    static final int inputLength = 2000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("input.txt"));
+        //BufferedReader is just a fast way to read input from a file
+
+        int[] depths = new int[inputLength];
+
+        for (int i = 0; i < inputLength; i++) {
+            depths[i] = Integer.parseInt(r.readLine());
+        }
+
+        int counter = 0;
+        for (int i = 1; i < inputLength; i++) {
+            if (depths[i] > depths[i - 1]) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+        r.close();
+    }
+}
